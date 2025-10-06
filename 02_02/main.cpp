c2e750c52520e481e7ca1aadbd40c0b5323c67b0
Hana-Suyama <115055069+Hana-Suyama@users.noreply.github.com>
2025-10-06 12:15:13 +0900

    02_02

---
+++ 02_02/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int RecursiveWage(int Time, int Wage, int AllWage) {
+	if (Time <= 1) {
+		return AllWage;
+	}
+	RecursiveWage(Time - 1, (Wage * 2) - 50, AllWage + (Wage * 2) - 50);
+}
+
+void WageCompare(int Time) {
+	printf("労働時間 : %d\n", Time);
+	printf("一般的な賃金体系 : %d\n", Time * 1226);
+	printf("再帰的な賃金体系 : %d\n", RecursiveWage(Time, 100, 100));
+}
+
+int main(){
+
+	WageCompare(8);
+
+	return 0;
+}
